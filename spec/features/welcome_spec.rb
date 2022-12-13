7e69cf93aa15cc157f68ce317f71d51c657ad0d1
Alexandre Moraes Tannus <alexandretannus@iMac-de-Alexandre.local>
2022-12-13 15:47:54 -0300

    :sparkles: Adição de botão de cadastro de clientes

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.feature "Welcomes", type: :feature do
   
   scenario 'Mostra a mensagem de boas vindas' do
-    visit('/')
+    visit(root_path)
 
     expect(page).to have_content('Bem Vindo')
   end
-  
+    
+  scenario 'Verificar o link Cadastro de Clientes' do
+    visit(root_path)
+
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
+  end
 end
