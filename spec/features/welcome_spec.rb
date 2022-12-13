ef1d1aceecd3c22479a5981e89af8d84007fd4f4
Alexandre Moraes Tannus <alexandretannus@iMac-de-Alexandre.local>
2022-12-13 13:49:04 -0300

    :sparkles: Adição de tela de boas vindas

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "Welcomes", type: :feature do
+  
+  scenario 'Mostra a mensagem de boas vindas' do
+    visit('/')
+
+    expect(page).to have_content('Bem Vindo')
+  end
+  
+end
